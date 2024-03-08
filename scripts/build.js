1f3e948888f5c8173a5b79d70b1b8c3582f52613
scil-t <116360839+scil-t@users.noreply.github.com>
2024-03-08 11:48:43 +0900

    using pnpm

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ const execa = require('execa');
 (async () => {
 	console.log('building packages/backend ...');
 
-	await execa('npm', ['run', 'build'], {
+	await execa('pnpm', ['run', 'build'], {
 		cwd: __dirname + '/../packages/backend',
 		stdout: process.stdout,
 		stderr: process.stderr,
@@ -11,7 +11,7 @@ const execa = require('execa');
 
 	console.log('building packages/client ...');
 
-	await execa('npm', ['run', 'build'], {
+	await execa('pnpm', ['run', 'build'], {
 		cwd: __dirname + '/../packages/client',
 		stdout: process.stdout,
 		stderr: process.stderr,
@@ -19,7 +19,7 @@ const execa = require('execa');
 
 	console.log('building packages/sw ...');
 
-	await execa('npm', ['run', 'build'], {
+	await execa('pnpm', ['run', 'build'], {
 		cwd: __dirname + '/../packages/sw',
 		stdout: process.stdout,
 		stderr: process.stderr,
@@ -27,7 +27,7 @@ const execa = require('execa');
 
 	console.log('build finishing ...');
 
-	await execa('npm', ['run', 'gulp'], {
+	await execa('pnpm', ['run', 'gulp'], {
 		cwd: __dirname + '/../',
 		stdout: process.stdout,
 		stderr: process.stderr,
